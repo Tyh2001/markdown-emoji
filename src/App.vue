bb6d3332367028b1e556723ba3fc2f7ba92ee22e
tyh2001 <1469442737@qq.com>
2022-08-24 15:46:14 +0800

    fix: 修复复制失败的问题

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script lang="ts" setup>
   import { emoji } from './json/emoji.json'
   import Clipboard from 'clipboard'
-  import { FMessage, FButton, FCard, FBackTop } from 'fighting-design'
-  import 'fighting-design/theme/message.css'
-  import 'fighting-design/theme/button.css'
+  import {
+    FMessage,
+    FButton,
+    FCard,
+    FBackTop,
+    FText,
+    FLink,
+  } from 'fighting-design'
 
   const copyCode = (node: string): void => {
     new Clipboard(node)
     FMessage({
       message: '复制成功',
-      type: 'primary',
+      type: 'success',
       round: true,
     })
   }
@@ -19,6 +24,18 @@
   <f-back-top>返回顶部</f-back-top>
 
   <f-card title="Markdown Emoji">
+    <f-text>
+      🤩 可在 Markdown 中使用的 emoji
+      表情，支持复制原表情和复制表情编码进行使用。
+    </f-text>
+    <f-link
+      target="_black"
+      type="primary"
+      url="https://github.com/Tyh2001/markdown-emoji"
+    >
+      Github
+    </f-link>
+
     <ul class="list">
       <li v-for="(item, key, index) in emoji" :key="index" class="item">
         <div class="emoji">{{ item }}</div>
@@ -29,7 +46,7 @@
             size="small"
             type="primary"
             :data-clipboard-text="`:${key}:`"
-            @click="copyCode('.left')"
+            @click="copyCode('.f-button-primary')"
           >
             复制代码
           </f-button>
@@ -38,7 +55,7 @@
             size="small"
             type="success"
             :data-clipboard-text="item"
-            @click="copyCode('.right')"
+            @click="copyCode('.f-button-success')"
           >
             复制表情
           </f-button>
